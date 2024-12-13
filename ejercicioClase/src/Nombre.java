07aacd8bc56645fe4f46a96a72e16b80bca069ba
Emanuel <zeroenygma2@gmail.com>
2024-12-13 18:06:53 +0000

    Update Nombre.java

--- ejercicioClase/src/Nombre.java
+++ ejercicioClase/src/Nombre.java
@@ -6,10 +6,14 @@ public class Nombre {
     private String nombre;
     private String apellido1;
     private String apellido2;
-
+    /**
+     *Constructor por defecto
+     */
     public Nombre() {
     }
-
+    /**
+     *Constructor con parametros
+     */
     public Nombre(String nombre, String apellido1, String apellido2) {
         this.nombre = nombre;
         this.apellido1 = apellido1;
